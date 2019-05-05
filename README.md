02eb74b30ea7d85fd6e96bf24492d9b731fc56a3
Serg4356 <asdfg111000@yandex.ru>
2019-05-05 19:25:32 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Devman task-control checking Telegram bot
+
+This simple telegram bot sends message every time when your task is checked on [dvmn.org](https://dvmn.org). It has two types of messages, depending on is there any mistakes found by your course tutor.
+
+### How to install
+
+python has to be installed on your system. Use pip (or pip3 if there is conflict with Python 2) to install dependences.
+```
+pip install -r requirements.txt
+```
+It is recommended to use virtual environment [virtualenv/venv](https://docs.python.org/3/library/venv.html) to isolate your project
+
+### Quickstart
+
+After installation type into console:
+```
+$python main.py
+```
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
--- main.py
+++ main.py
@@ -2,30 +2,22 @@ import requests
 from dotenv import load_dotenv
 import os
 import telegram
-from pprint import pprint
 
 
-def check_devman(dvmn_token):
-    url = 'https://dvmn.org/api/long_polling/'
-    headers = {
-        'Authorization': f'Token {token}',
-    }
-    params = {}
-    while True:
-        try:
-            response = requests.get(url,
-                                    params=params,
-                                    headers=headers,
-                                    timeout=120)
-            print(response.json())
-            params['timestamp'] = response.json()['timestamp_to_request']
-        except requests.exceptions.ReadTimeout:
-            pass
-        except requests.exceptions.ConnectionError:
-            pass
-        except KeyError:
-            params['timestamp'] = response.json()['last_attempt_timestamp']
 
+def make_bot_messages(attempts):
+    bot_messages = []
+    for attempt in attempts:
+        bot_message = ''
+        lesson_title = attempt['lesson_title']
+        bot_message += f'У вас проверили работу {lesson_title}\n\n'
+        if attempt['is_negative']:
+            bot_message += 'К сожалению, в работе нашлись ошибки'
+        else:
+            bot_message += 'Преподавателю все понравилось, можно'
+            ' приступать к следующему уроку'
+        bot_messages.append(bot_message)
+    return bot_messages
 
 
 if __name__ == '__main__':
@@ -33,8 +25,28 @@ if __name__ == '__main__':
     dvmn_token = os.getenv('dvmn_token')
     telegram_token = os.getenv('telegram_token')
     http_proxy = os.getenv('HTTP_PROXY')
-    chat_id = 390121493
-    bot_request = telegram.utils.request.Request(proxy_url=http_proxy)
-    bot = telegram.Bot(token=telegram_token, request=bot_request)
-    bot.send_message(chat_id=chat_id, text='Света, жопа')
-
+    chat_id = os.getenv('chat_id')
+    bot_proxy = telegram.utils.request.Request(proxy_url=http_proxy)
+    bot = telegram.Bot(token=telegram_token, request=bot_proxy)
+    url = 'https://dvmn.org/api/long_polling/'
+    headers = {
+        'Authorization': f'Token {dvmn_token}',
+    }
+    params = {}
+    while True:
+        try:
+            response = requests.get(url,
+                                    params=params,
+                                    headers=headers,
+                                    timeout=91)
+            if 'new_attempts' in response.json().keys():
+                bot_messages = make_bot_messages(response.json()['new_attempts'])
+                for bot_message in bot_messages:
+                    bot.send_message(chat_id=chat_id, text=bot_message)
+                params['timestamp'] = response.json()['last_attempt_timestamp']
+                continue
+            params['timestamp'] = response.json()['timestamp_to_request']
+        except requests.exceptions.ReadTimeout:
+            pass
+        except requests.exceptions.ConnectionError:
+            pass
