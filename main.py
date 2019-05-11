d6e3354305bf326379fd75e28253ce2af6da83d8
Serg4356 <asdfg111000@yandex.ru>
2019-05-11 19:17:21 +0500

    Add logging

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
+import logging
 from dotenv import load_dotenv
 import os
 import telegram
 
 
 def make_bot_message(attempt):
-    bot_message = 'У вас проверили работу {}\n\n'.format(
+    bot_message = 'У вас проверили работу: "{}"\n\n'.format(
         attempt['lesson_title'])
     if attempt['is_negative']:
         bot_message += 'К сожалению, в работе нашлись ошибки'
@@ -29,14 +30,18 @@ if __name__ == '__main__':
         'Authorization': f'Token {dvmn_token}',
     }
     params = {}
+    logging.basicConfig(format=('%(asctime)s %(process)d %(levelname)s %(message)s'))
+    logging.info('Bot has been started')
     while True:
         try:
             response = requests.get(url,
                                     params=params,
                                     headers=headers,
                                     timeout=91)
+            logging.info('Sent request to Devman')
             dvmn_check_info = response.json()
             if 'new_attempts' in dvmn_check_info.keys():
+                logging.info('New attempts found')
                 bot_messages = []
                 for attempt in dvmn_check_info['new_attempts']:
                     bot_messages.append(make_bot_message(attempt))
@@ -44,8 +49,9 @@ if __name__ == '__main__':
                     bot.send_message(chat_id=chat_id, text=bot_message)
                 params['timestamp'] = dvmn_check_info['last_attempt_timestamp']
                 continue
+            logging.info('No new attempts found')
             params['timestamp'] = dvmn_check_info['timestamp_to_request']
-        except requests.exceptions.ReadTimeout:
-            pass
-        except requests.exceptions.ConnectionError:
-            pass
+        except requests.exceptions.ReadTimeout as read_timeout_err:
+            logging.error(read_timeout_err, exc_info=True)
+        except requests.exceptions.ConnectionError as conn_err:
+            logging.error(conn_err, exc_info=True)
